bafb3dd9c502f3dcecd014e204de586ef8df7d8f
yoshiya-m <2101115016rz@cyber-u.ac.jp>
2024-11-07 22:46:04 +0900

    update header.php

--- Views/layout/header.php
+++ Views/layout/header.php
@@ -11,7 +11,7 @@ use Helpers\Settings;
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="/css/style.css">
-    <title>5min Tetris</title>
+    <title>1min Tetris</title>
 </head>
 
 <body>
